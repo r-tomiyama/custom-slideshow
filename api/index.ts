f0f9e837857f126825663d996a9fa50e9713a4f2
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-08-21 18:09:00 +0900

    add linter

--- api/index.ts
+++ api/index.ts
@@ -5,18 +5,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.listen(3000, () => {
-    console.log("Start on port 3000.")
+  console.log('Start on port 3000.')
 })
 
 type image = {
-    id: number
-    url: string
-};
+  id: number
+  url: string
+}
 
-const images: image[] = [
-    { id: 1, url: "hoge.com" }
-]
+const images: image[] = [{ id: 1, url: 'hoge.com' }]
 
 app.get('/images', (req: express.Request, res: express.Response) => {
-    res.send(JSON.stringify(images))
+  res.send(JSON.stringify(images))
 })
